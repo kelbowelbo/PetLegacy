627fc580cf559f5a31ccabbd6d0f32328f4fd065
Kelly Wenzel <aspenmarch@gmail.com>
2018-01-03 12:43:49 -0700

    added components

---
+++ petLegacyfrontend/src/components/search.js
@@ -0,0 +1,7 @@
+<Row>
+		<Input placeholder="Placeholder" s={6} label="First Name" />
+		<Input s={6} label="Last Name" />
+		<Input s={12} label="disabled" defaultValue="I am not editable" disabled />
+		<Input type="password" label="password" s={12} />
+		<Input type="email" label="Email" s={12} />
+</Row>
